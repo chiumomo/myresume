cbbde1221712bbf7038b38ae1a554c23b83bd4e2
Momo Chiu <q10110144kh@gmail.com>
2023-08-27 03:48:06 +0800

    change var wow

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ root.render(
     <style>
       @import url('https://fonts.googleapis.com/css2?family=Indie+Flower&family=Noto+Sans+TC:wght@300;400;500&family=Roboto+Condensed:ital,wght@0,300;0,400;0,700;1,300;1,400&family=Roboto:wght@300;400&display=swap');
     </style>
+    
     <App />
   </BrowserRouter>
 );
