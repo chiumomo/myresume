1c37fbf25d59171c7d23c29818904511761d08cd
chiumomo <q10110144kh@gmail.com>
2025-05-18 18:56:56 +0800

    Updates

---
+++ precache-manifest.91b799c2d07b629d36a4bfe84938f959.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc379d7dfd8beed0440fe61b1ad7dae7",
+    "url": "/myresume/index.html"
+  },
+  {
+    "revision": "d2db57b913d16a7fa66a",
+    "url": "/myresume/static/css/2.27754eeb.chunk.css"
+  },
+  {
+    "revision": "3ffeaa3f2957fd72b300",
+    "url": "/myresume/static/css/main.f3ad3385.chunk.css"
+  },
+  {
+    "revision": "d2db57b913d16a7fa66a",
+    "url": "/myresume/static/js/2.c4bbbe8d.chunk.js"
+  },
+  {
+    "revision": "08777cbbe06cbc652710",
+    "url": "/myresume/static/js/3.d55765a0.chunk.js"
+  },
+  {
+    "revision": "3ffeaa3f2957fd72b300",
+    "url": "/myresume/static/js/main.7811a744.chunk.js"
+  },
+  {
+    "revision": "4cb9ef2c0bcd62b3d542",
+    "url": "/myresume/static/js/runtime~main.834d4b62.js"
+  },
+  {
+    "revision": "e9e6fac335a75ad67f71338b2b7ec7f3",
+    "url": "/myresume/static/media/01.e9e6fac3.jpg"
+  },
+  {
+    "revision": "3cbfa78528ad27bfa633a2be16a2ea84",
+    "url": "/myresume/static/media/02.3cbfa785.jpg"
+  },
+  {
+    "revision": "2edaad52dfb1f0e4a522c4953ad87379",
+    "url": "/myresume/static/media/03.2edaad52.jpg"
+  },
+  {
+    "revision": "bc52b2206b355adbf2b3239c2f367541",
+    "url": "/myresume/static/media/04.bc52b220.jpg"
+  },
+  {
+    "revision": "b3fbb200d6e8c78676200fc414b3192b",
+    "url": "/myresume/static/media/05.b3fbb200.jpg"
+  },
+  {
+    "revision": "df66e09a9868fd1f7b7af122e7d37eb5",
+    "url": "/myresume/static/media/06.df66e09a.jpg"
+  },
+  {
+    "revision": "f94912c4845855e8211f5bc512acb9bd",
+    "url": "/myresume/static/media/07.f94912c4.jpg"
+  },
+  {
+    "revision": "4bfea6b121b5bd3c9b9a6bf4dbe67eac",
+    "url": "/myresume/static/media/Header.4bfea6b1.png"
+  },
+  {
+    "revision": "5625753847ac19e2f541c8ef7f41e126",
+    "url": "/myresume/static/media/Javascript-logo-basico.56257538.png"
+  },
+  {
+    "revision": "bbfa3cc950b609a0f202fa8125ba4fff",
+    "url": "/myresume/static/media/UIUXdisney.bbfa3cc9.png"
+  },
+  {
+    "revision": "1c1f8f65404c4f449a382b2de7bc36a1",
+    "url": "/myresume/static/media/UIUXtablegame.1c1f8f65.png"
+  },
+  {
+    "revision": "4d69a020b08a10e7dd866b5961c62f86",
+    "url": "/myresume/static/media/bigleaf.4d69a020.png"
+  },
+  {
+    "revision": "c9421be52fe48fdf04839b53f8e9f1d4",
+    "url": "/myresume/static/media/css-logo.c9421be5.png"
+  },
+  {
+    "revision": "94e2ea60b6359a09cbeea4d19e7b0635",
+    "url": "/myresume/static/media/figma-logo.94e2ea60.png"
+  },
+  {
+    "revision": "d288810b3460e84091aceb7575735c1e",
+    "url": "/myresume/static/media/frontendStore1.d288810b.png"
+  },
+  {
+    "revision": "c6b8df56c419e821e732948e1d565896",
+    "url": "/myresume/static/media/frontendStore2.c6b8df56.png"
+  },
+  {
+    "revision": "cee58f4e4c4028f1e9d1de9ccef25f8f",
+    "url": "/myresume/static/media/frontendStore3.cee58f4e.png"
+  },
+  {
+    "revision": "7b06dd710c1c014bae24c9b0989ede2d",
+    "url": "/myresume/static/media/frontendmyresume.7b06dd71.png"
+  },
+  {
+    "revision": "d5b9f1715053fdb2a836a3feb4b8dca6",
+    "url": "/myresume/static/media/html-logo.d5b9f171.png"
+  },
+  {
+    "revision": "ce1f09b35ca48066938542a45d03b7e0",
+    "url": "/myresume/static/media/react-logo2.ce1f09b3.png"
+  },
+  {
+    "revision": "ed68c3aa46cfc876630cd87ed5a664af",
+    "url": "/myresume/static/media/sass.ed68c3aa.png"
+  },
+  {
+    "revision": "1b6443cc46a3c4f46fb9f6a018a742b5",
+    "url": "/myresume/static/media/sticker1.1b6443cc.png"
+  },
+  {
+    "revision": "2e290e41139d1b6d3d0ae0c97db14c99",
+    "url": "/myresume/static/media/sticker2.2e290e41.png"
+  },
+  {
+    "revision": "af629b3d27e7410f7451b54b9f201a8c",
+    "url": "/myresume/static/media/sticker3.af629b3d.png"
+  }
+]);
